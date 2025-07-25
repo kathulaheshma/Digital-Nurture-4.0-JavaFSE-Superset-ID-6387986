5d4deba975109ade7ad8f512f6c5b7ed7d9705f4
kathulaheshma <kathulaheshma.4@gmail.com>
2025-07-25 22:03:27 +0530

    Delete shoppingapp directory

--- shoppingapp/src/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import OnlineShopping from "./OnlineShopping";
-
-ReactDOM.render(<OnlineShopping />, document.getElementById("root"));
